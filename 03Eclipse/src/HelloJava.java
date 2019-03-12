a308c31fbddc438a8173fec1299ade16e28f1799
colourstar <colourstar27@qq.com>
2019-03-12 14:27:31 +0800

    first eclipse project

---
+++ 03Eclipse/src/HelloJava.java
@@ -0,0 +1,13 @@
+
+public class HelloJava 
+{
+	public static void main(String[] args) 
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			System.out.println("Hello Java");
+			System.out.println("Hello Java1");
+			System.out.println("Hello Java2");			
+		}
+	}
+}
